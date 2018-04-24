02d1c075bd1e3a14a49d4f1677dd3f79c90fbe31
AlexDeveloper24 <alexchubukov12345@gmail.com>
2018-04-25 00:41:09 +0300

    tests

--- tests/tree_test.cpp
+++ tests/tree_test.cpp
@@ -1,9 +1,15 @@
 #include <catch.hpp>
 #include <sstream>
 #include "tree.h"
+using namesapce BSTree;
 
-TEST_CASE("tree output")
+TEST_CASE("insert")
 {
-
+	Tree tree;
+	tree.insert(6);
+	tree.insert(2);
+	tree.insert(8);
+	REQUIRE( tree.insert(3)==1);
+	REQUIRE( tree.insert(3)==0);
 }
 
